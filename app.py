62fb82de4ea9b5e7834ff64cf7c6e43fb832074e
Gilad Zur <gilad.zur90@gmail.com>
2022-03-25 12:00:43 +0300

    new web page + GET & POST

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from datetime import date
+import time
 from DAL import *
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello DevOps Class</h1>'
+    return '<h1>Welcome to my Inventory!</h1>'
 
 @app.route('/user/<name>')
 def user(name):
@@ -13,9 +16,21 @@ def user(name):
 
 @app.route('/inventory/')
 def inventory():
-    return f'<h1> {get_items()} </h1>'
+    return F'<html><body><h1> { get_items()}<h1><body><html>'
+
+@app.route('/insertitem', methods=['GET', 'POST'])  # GET REQUEST
+def load_insert_item_html():
+    if request.method == 'POST':
+        data = request.form['name']
+        data2 = request.form['category']
+        data3 = request.form['quantity']
+        data4 = request.form['price']
+        insert_item(data, data2, data3, data4,date.today())      
+        return render_template('insertitem.html', data=data)
+    
+    return render_template('insertitem.html')
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
 
 
